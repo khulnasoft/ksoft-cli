a6b3765430e5aaa2472780142b69108271b87db8
NxPKG <116948796+NxPKG@users.noreply.github.com>
2025-02-16 02:42:55 +0600

    Update database.py
    Signed-off-by: NxPKG <116948796+NxPKG@users.noreply.github.com>

--- ksoft/core/database.py
+++ ksoft/core/database.py
@@ -8,12 +8,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-# Example function to add a new module
-def add_module(db: Session, name: str, description: str = None):
-    from ksoft.api.models import Module
-    new_module = Module(name=name, description=description)
-    db.add(new_module)
-    db.commit()
-    db.refresh(new_module)
-    return new_module
